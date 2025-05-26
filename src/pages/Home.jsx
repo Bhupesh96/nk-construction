8cd5b799cb6c952d1d7546ef8f8037bff132f372
Bhupesh Sahu <bhupeshsahu9617@gmail.com>
2025-05-26 15:53:07 +0530

    testing deployment again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,6 @@ const Home = () => {
         <div className="preloader">
           <div className="icon" />
         </div>
-        <div>Testing Deployment</div>
         <Header />
         <div id="search-popup" className="search-popup">
           <div className="close-search theme-btn">
